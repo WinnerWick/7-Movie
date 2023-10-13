005b869bda9199eaf6f40ddc8b4e9a7c72c98be0
Wuanderson Winner <50545980+WinnerWick@users.noreply.github.com>
2023-10-13 10:18:49 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+let menu = document.getElementById("menu");
+let toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", function () {
+  menu.classList.toggle("open");
+});
+
+function poster1() {
+  window.open("https://youtu.be/PgrmbRID0eY?si=UO0lC-olq8qycDr2");
+}
+function poster2() {
+  window.open("https://youtu.be/IqvRDhW-XVA?si=B0Dc3yCjqIf26h8l");
+}
+function poster3() {
+  window.open("https://youtu.be/1b8ikRAtXHU?si=4Cs0CiMo91yJsh-L");
+}
+function poster4() {
+  window.open("https://youtu.be/bTZucJi2Z3A?si=MlJhxKH1EbZ8Nm7D");
+}
+function poster5() {
+  window.open("https://youtu.be/HKVve_VSz58?si=WPpVZ9FKNG7en97D");
+}
+function poster6() {
+  window.open("https://youtu.be/bpAc_FLxdhI?si=TsF4eihjXdfZsQDY");
+}
+function poster7() {
+  window.open("https://youtu.be/RMmGQNNl164?si=Lo_HB1DkbDOtB9QT");
+}
+function poster8() {
+  window.open("https://youtu.be/TN-_xS4Kdpg?si=h67mO4HdwtFR9YTh");
+}
+function poster9() {
+  window.open("https://youtu.be/7z7xJcOTja0?si=gezsg0vNpRoWqkfL");
+}
+function poster10() {
+  window.open("https://youtu.be/e4yH53G6kM8?si=nzraeMnqZDzPLfgL");
+}
+function poster11() {
+  window.open("https://youtu.be/Poo5lqoWSGw?si=7DTxG4JewsUWZPpv");
+}
+function poster12() {
+  window.open("https://youtu.be/-G9BqkgZXRA?si=pKgey5tdyTRlXNv1");
+}
